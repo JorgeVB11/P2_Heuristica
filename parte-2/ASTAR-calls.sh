9815c6ba3ebee70b0b982846d4c3e2969eb0b88b
jvina <jvinasblasco@gmail.com>
2023-12-15 23:50:32 +0100

    más cosas

--- parte-2/ASTAR-calls.sh
+++ parte-2/ASTAR-calls.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 python3 ASTARTraslados.py ASTAR-tests/mapa-1.csv 1
 python3 ASTARTraslados.py ASTAR-tests/mapa-2.csv 1
+python3 ASTARTraslados.py ASTAR-tests/mapa-3.csv 2
+python3 ASTARTraslados.py ASTAR-tests/mapa-4.csv 3
+python3 ASTARTraslados.py ASTAR-tests/mapa-5.csv 4
